ebac299f4b5282c1acefcb3cfca3a2ec47f1af5a
PadawanMiyagi <Jacob@Bennedsen.dk>
2018-10-06 18:01:25 +0200

    Added Neopixel and ESP8266

---
+++ Adafruit_and_ESP/Adafruit_and_ESP.ino
@@ -0,0 +1,159 @@
+// Adafruit_NeoMatrix example for single NeoPixel Shield.
+// Scrolls 'Howdy' across the matrix in a portrait (vertical) orientation.
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
+
+#define PIN 10
+
+// MATRIX DECLARATION:
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+
+
+// Example for NeoPixel Shield.  In this application we'd like to use it
+// as a 5x8 tall matrix, with the USB port positioned at the top of the
+// Arduino.  When held that way, the first pixel is at the top right, and
+// lines are arranged in columns, progressive order.  The shield uses
+// 800 KHz (v2) pixels that expect GRB color data.
+
+String textValue = ""; // Text to be shown on display
+const char* ssid     = "Delorean"; // SSID for WiFi
+const char* password = "AssJack2"; // Password for WiFi
+
+ESP8266WebServer server ( 80 ); // Start server on port 80
+char htmlResponse[3000]; // Length of text response
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
+  NEO_MATRIX_BOTTOM  + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+// Above describes the display layout @Jacob .......
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+
+void handleRoot() {
+
+  snprintf ( htmlResponse, 3000,
+"<!DOCTYPE html>\
+<html lang=\"en\">\
+  <head>\
+    <meta charset=\"utf-8\">\
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
+  </head>\
+  <body style=\"backgroundColor:\#ECFE1D\">\
+          <h1>Skriv din tekst her</h1>\
+          <input type='text' name='text' id='text' size=20 autofocus> \
+          <div>\
+          <br><button id=\"save_button\">Save</button>\
+          </div>\
+    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>\    
+    <script>\
+      var hh;\
+      var mm;\
+      var ss;\
+      $('#save_button').click(function(e){\
+        e.preventDefault();\
+        text = $('#text').val();\     
+        $.get('/save?text=' + text, function(data){\
+          console.log(data);\
+        });\
+      });\      
+    </script>\
+  </body>\
+</html>"); 
+
+   server.send ( 200, "text/html", htmlResponse );  
+
+}
+
+void handleSave() {
+  if (server.arg("text")!= ""){
+    if(textValue != server.arg("text")){
+        Serial.println("Text der bliver vist på displayet: '" + server.arg("text") + "'");
+        textValue = server.arg("text");
+      }
+  }
+}
+
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(colors[0]);
+
+  // Setup for web server
+  // Start serial
+  Serial.begin(115200);
+  delay(10);
+
+  // Connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  // Print the IP of the webPage to the display
+  textValue = String(WiFi.localIP());
+
+  server.on ( "/", handleRoot );
+  server.on ("/save", handleSave);
+
+  server.begin();
+  Serial.println ( "HTTP server started" );
+}
+
+int x    = matrix.width();
+int pass = 0;
+
+void loop() {
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  // matrix.print(F("Howdy"));
+  matrix.print(String(textValue));
+  //matrix.print(F("TJ Bot...."));
+  if(--x < -36) {
+    x = matrix.width();
+    if(++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
+  delay(100);
+  server.handleClient();
+}
+
+
